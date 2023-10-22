b078a926e15378d3e9d1bf42b77f2321811badab
Eduard Suciu <eduard.suciu@itmaniax.com>
2023-10-22 13:39:48 +0300

    Added macro keybind

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,9 +1,5 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
--- See the kickstart.nvim README for more information
-
-vim.api.nvim_set_keymap('x', '@', ':normal @q<CR>', { noremap = true, silent = true })
-
 
 return {}
---
+++ lua/custom/plugins/macro.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap('x', '@', ':normal @q<CR>', { noremap = true, silent = true })
+
+return {}
